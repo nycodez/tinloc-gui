288bef266b523593619863004b49fd90fd30d7f7
agonzales <gonzales.andres@gmail.com>
2020-05-06 16:04:36 +0700

    Adding files

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
   <div id="app">
     <b-navbar toggleable="md" type="dark" variant="dark">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand to="/">My Vue App</b-navbar-brand>
+      <b-navbar-brand to="/">TinLoc</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/matches">Match Manager</b-nav-item>
+          <b-nav-item to="/matches">Matches</b-nav-item>
           <b-nav-item href="#" @click.prevent="login" v-if="!activeUser">Login</b-nav-item>
           <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
         </b-navbar-nav>
